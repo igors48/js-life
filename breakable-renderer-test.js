6d03d160295dc6d713a4b133dd6824b2080fc7a4
Igor Usenko <nmds48@gmail.com>
2013-03-22 23:41:54 +0200

    first implementation of renderer

---
+++ breakable-renderer-test.js
@@ -0,0 +1,103 @@
+var BreakableRendererTestTools = BreakableRendererTestTools || {};
+
+BreakableRendererTestTools.createModel = function () {
+    "use strict";
+    
+    return [new Coordinates(48, 48), new Coordinates(48, 48), new Coordinates(48, 48), new Coordinates(48, 48), new Coordinates(48, 48)];
+};
+    
+BreakableRendererTestTools.assertModelPainted = function (renderer, painter) {
+    "use strict";
+    
+    var completed = renderer.paintPart();
+    var paintedCellCount = painter.getPaintedCellCount();
+    
+    ok(!completed, "Painting is not completed");
+    equal(paintedCellCount, 2, "Painted cell count is valid");
+
+    completed = renderer.paintPart();
+    paintedCellCount = painter.getPaintedCellCount();
+    
+    ok(!completed, "Painting is not completed");
+    equal(paintedCellCount, 4, "Painted cell count is valid");
+
+    completed = renderer.paintPart();
+    paintedCellCount = painter.getPaintedCellCount();
+    
+    ok(completed, "Painting is completed");
+    equal(paintedCellCount, 5, "Painted cell count is valid");
+};    
+
+test("Empty model does not draw", function() {
+    "use strict";
+    
+    var painter = new PainterStub();
+    var renderer = new BreakableRenderer(4, painter);
+    
+    var completed = renderer.paintPart();
+    var paintedCellCount = painter.getPaintedCellCount();
+    
+    ok(completed, "Draw completed after first call");
+    equal(paintedCellCount, 0, "No cell was painted");
+});
+
+test("When new model replaced painter cleared", function() {
+    "use strict";
+    
+    var painter = new PainterStub();
+    var renderer = new BreakableRenderer(4, painter);
+    
+    var model = [new Coordinates(48, 48)];
+    renderer.replaceModel(model);
+    
+    var cleared = painter.isCleared();
+    
+    ok(cleared, "Painter was cleared");
+});
+
+test("Restarting renderer cleared painter", function() {
+    "use strict";
+    
+    var painter = new PainterStub();
+    var renderer = new BreakableRenderer(4, painter);
+    
+    renderer.restart();
+    
+    var cleared = painter.isCleared();
+    
+    ok(cleared, "Painter was cleared");
+});
+
+test("Painting process breaks into parts", function() {
+    "use strict";
+    
+    var painter = new PainterStub();
+    var renderer = new BreakableRenderer(2, painter);
+    
+    var model = BreakableRendererTestTools.createModel();
+    renderer.replaceModel(model);
+
+    BreakableRendererTestTools.assertModelPainted(renderer, painter);
+});
+
+test("Painting process really restarted", function() {
+    "use strict";
+    
+    var painter = new PainterStub();
+    var renderer = new BreakableRenderer(2, painter);
+    
+    var model = BreakableRendererTestTools.createModel();
+    renderer.replaceModel(model);
+
+    var completed = renderer.paintPart();
+    var paintedCellCount = painter.getPaintedCellCount();
+    
+    ok(!completed, "Painting is not completed");
+    equal(paintedCellCount, 2, "Painted cell count is valid");
+
+    renderer.restart();
+    
+    BreakableRendererTestTools.assertModelPainted(renderer, painter);
+});
+
+
--- breakable-renderer.js
+++ breakable-renderer.js
@@ -1,19 +1,50 @@
 var BreakableRenderer = function (paintPerCall, painter) {
     "use strict";
+
+    Assert.isPositiveInteger(paintPerCall);
+    this._paintPerCall = paintPerCall;
     
     Assert.isNotNullAndDefined(painter);
     this._painter = painter;
+    
+    this._model = [];
+    this._index = 0;
 };
 
 BreakableRenderer.prototype = {
 
     replaceModel: function (model) {
+        "use strict";
+
+        Assert.isArray(model);
+        this._model = model;
+
+        this.restart();    
     },
     
     paintPart: function () {
+        "use strict";
+        
+        var count = 0;
+        var maxIndex = this._model.length;
+        
+        while (count < this._paintPerCall && this._index < maxIndex) {
+            var current = this._model[this._index];
+            
+            this._painter.paintCell(current);
+            
+            ++this._index;
+            ++count;
+        }
+        
+        return this._index === maxIndex;
     },
     
-    reset: function () {
+    restart: function () {
+        "use strict";
+        
+        this._painter.clear();
+        this._index = 0;
     }
     
 };
