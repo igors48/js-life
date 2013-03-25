aefd5456c2f5b4ac52eb18f97b3ca8611033e8a8
Igor Usenko <nmds48@gmail.com>
2013-03-25 14:40:07 +0200

    another enhancement

--- field.js
+++ field.js
@@ -25,34 +25,37 @@ Field.prototype = {
         "use strict";
 
         var previousGenerationHabitat = this._cells.habitat();    
-        var cells = this._cells.cells();
-        var map = new CellsList();
-        var that = this;
+		
+        var processedCells = new CellsList();
+        var newModel = new CellsList();
         
         var livingCells = [];        
         var borningCells = [];
         var dyingCells = [];
 
+        var cells = this._cells.cells();
+        var that = this;
+
         _.each(cells,
             function (cell) {
-                that._processLiveCell(cell, map, livingCells, borningCells, dyingCells);
+                that._processLiveCell(cell, processedCells, livingCells, borningCells, dyingCells, newModel);
             }
         );
 
-        this._updateCells(livingCells, borningCells);
+		this._cells = newModel;
         
         var generationReport = new GenerationReport(borningCells, dyingCells, livingCells, previousGenerationHabitat);
         
         return generationReport;
     },
 
-    _processLiveCell: function (cell, map, livingCells, borningCells, dyingCells) {
+    _processLiveCell: function (cell, processedCells, livingCells, borningCells, dyingCells, newModel) {
         "use strict";
 
         var x = cell.coordinates().x();    
         var y = cell.coordinates().y();
         
-        this._storeNeighborsCountAndCellState(x, y, true, map, livingCells, borningCells, dyingCells);
+        this._storeNeighborsCountAndCellState(x, y, true, processedCells, livingCells, borningCells, dyingCells, newModel);
 
         var coordinates = new Coordinates(x, y);    
         var neighbors = Neighbors.getNeighbors(coordinates);
@@ -61,29 +64,29 @@ Field.prototype = {
         
         _.each(neighbors,
             function (current) {
-                that._storeNeighborsCountAndCellState(current.x(), current.y(), false, map, livingCells, borningCells, dyingCells);
+                that._storeNeighborsCountAndCellState(current.x(), current.y(), false, processedCells, livingCells, borningCells, dyingCells, newModel);
             }
         );        
     },
 
-    _storeNeighborsCountAndCellState: function (x, y, liveCell, map, livingCells, borningCells, dyingCells) {
+    _storeNeighborsCountAndCellState: function (x, y, liveCell, processedCells, livingCells, borningCells, dyingCells, newModel) {
 
-        if (map.exists(x, y)) {
+        if (processedCells.exists(x, y)) {
             return;
         }
         
         var neighborsCount = this._countNeighbors(x, y);
         var isEmpty = liveCell ? false : !this._cells.exists(x, y);
         
-        var neighborsAndStatus = new NeighborsAndState(neighborsCount, isEmpty);
-        
-        map.add(x, y, neighborsAndStatus);
+        processedCells.add(x, y);
 		
 		if (isEmpty) {
                 
             if (neighborsCount === 3) {
                 var borningCell = new Coordinates(x, y);
                 borningCells.push(borningCell);
+
+				newModel.add(x, y);
             }
         } else {
                 
@@ -95,30 +98,12 @@ Field.prototype = {
             if (neighborsCount === 2 || neighborsCount === 3) {
                 var livingCell = new Coordinates(x, y);
                 livingCells.push(livingCell);
+
+				newModel.add(x, y);
             }
         }
     },
-    
-    _updateCells: function (livingCells, borningCells) {
-        "use strict";
 
-        this._cells = new CellsList();
-
-        var that = this;
-        
-        _.each(livingCells,
-            function (cell) {
-				that._cells.add(cell.x(), cell.y());
-            }
-        );
-
-        _.each(borningCells,
-            function (cell) {
-				that._cells.add(cell.x(), cell.y());
-            }
-        );
-    },
-    
     _countNeighbors: function (x, y) {
         "use strict";
 
